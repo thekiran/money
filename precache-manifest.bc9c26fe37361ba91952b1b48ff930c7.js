a9c8e1b0a5bfbdef02562ee55a264500f0f05cc3
unknown <kirankumargs23.kkgs@gmail.com>
2020-11-08 15:45:09 +0530

    Updates

---
+++ precache-manifest.bc9c26fe37361ba91952b1b48ff930c7.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fc3bdad119b548e82fb402d1eeacb77",
+    "url": "/money/index.html"
+  },
+  {
+    "revision": "a59940a9e12d1ab0c3d7",
+    "url": "/money/static/css/main.5d5c5b0c.chunk.css"
+  },
+  {
+    "revision": "c00e42796b051794d6bb",
+    "url": "/money/static/js/2.7008cb6d.chunk.js"
+  },
+  {
+    "revision": "a51e9d6930fab2afb4bd44dab85916d3",
+    "url": "/money/static/js/2.7008cb6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a59940a9e12d1ab0c3d7",
+    "url": "/money/static/js/main.3edcc23e.chunk.js"
+  },
+  {
+    "revision": "d185d1b1be10bc5f1891",
+    "url": "/money/static/js/runtime-main.bfc33afe.js"
+  },
+  {
+    "revision": "3e6a0f219883c5a923f2dd501e1a8ee1",
+    "url": "/money/static/media/about.3e6a0f21.jpg"
+  },
+  {
+    "revision": "f38f0e6ad96caf4be73c52ac06effc67",
+    "url": "/money/static/media/about.f38f0e6a.svg"
+  },
+  {
+    "revision": "4c6b5dcc4cf6d92f363d50da5d745468",
+    "url": "/money/static/media/apple.4c6b5dcc.svg"
+  },
+  {
+    "revision": "49c9d684ae0eaf203ac650430dd11a6a",
+    "url": "/money/static/media/bank.49c9d684.svg"
+  },
+  {
+    "revision": "57cf6581889db7d3210268530bce0065",
+    "url": "/money/static/media/brand.57cf6581.png"
+  },
+  {
+    "revision": "f64bde514d1f8ce730c3980f7fe1d142",
+    "url": "/money/static/media/calculator.f64bde51.svg"
+  },
+  {
+    "revision": "6ebfab22d1aa9c0f05c9b83312cfd38e",
+    "url": "/money/static/media/carousel1.6ebfab22.jpeg"
+  },
+  {
+    "revision": "847b9f130367659c293203014d1fb693",
+    "url": "/money/static/media/carousel2.847b9f13.jpeg"
+  },
+  {
+    "revision": "96e9df2b408581bad53eca72afb4d6b2",
+    "url": "/money/static/media/carousel3.96e9df2b.jpeg"
+  },
+  {
+    "revision": "3bb11a40cbe37f4b2ca1c00cc39d5f3c",
+    "url": "/money/static/media/money.3bb11a40.svg"
+  },
+  {
+    "revision": "a53de91e2a5d94b0be6af17b03224217",
+    "url": "/money/static/media/play.a53de91e.svg"
+  },
+  {
+    "revision": "447e03c304f5697f2dd1fcd12aa8a1b8",
+    "url": "/money/static/media/report.447e03c3.svg"
+  },
+  {
+    "revision": "4473d828b6a7bff16c3f711a262bf1d3",
+    "url": "/money/static/media/school.4473d828.svg"
+  }
+]);
